3531eecb72c6d9beffd26025c31c770c2612fec9
Eduardo Piassaroli de Abreu <146479077+EduardoPia@users.noreply.github.com>
2023-11-24 12:56:58 -0300

    Delete Bomberman directory

--- Bomberman/cena_inicial.py
+++
@@ -1,4 +0,0 @@
-
-
-class CenaInicial:
-    pass
--- Bomberman/main.py
+++
@@ -1,9 +0,0 @@
-
-from bomberman import Bomberman
-
-def main():
-    jogo = Bomberman()
-    jogo.executar()
-
-if __name__ == "__main__":
-    main()
